1a70ab56fda9d110248a438cb0f601b935535cf3
Takamori Yamashiro <takamori@yama-lab.com>
2018-07-10 04:06:24 +0900

    add functions

--- src/BlaynMail.php
+++ src/BlaynMail.php
@@ -5,8 +5,11 @@ namespace takamoriyamashiro\Blayn;
 class BlaynMail
 {
 	
-	private $access_token = false;
 	const REQUEST_OPTIONS = ['encoding' => 'UTF-8', 'escaping' => 'markup'];
+
+	private $access_token = false;
+	private $errors = [];
+	
 	
 	public function __construct($id, $password, $apikey)
 	{
@@ -26,10 +29,11 @@ class BlaynMail
 		$file = file_get_contents("https://api.bme.jp/xmlrpc/1.0", false, $context);
 		$response = xmlrpc_decode($file);
 		$this->access_token = false;
+		$this->errors = [];
 		return $response;
 	}
 	
-	private function makeContext($request,$method='POST',$header="Content-Type: text/xml")
+	private function makeContext($request, $method = 'POST', $header = "Content-Type: text/xml")
 	{
 		$context = stream_context_create([
 			'http' => [
@@ -52,12 +56,69 @@ class BlaynMail
 		if (empty($email) || empty($group) || $this->access_token === false) {
 			return false;
 		}
-		return false;
-		$params = [$this->access_token, ['c15' => $email,'c21'=>$group]];
+		$params = [$this->access_token, ['c15' => $email, 'c21' => $group]];
 		$request = xmlrpc_encode_request('contact.detailCreate', $params, self::REQUEST_OPTIONS);
 		$context = $this->makeContext($request);
 		$file = file_get_contents("https://api.bme.jp/xmlrpc/1.0", false, $context);
 		$data = xmlrpc_decode($file);
-		
+		if($this->checkError($data)){
+			return false;
+		}
+		return $data;
 	}
+	
+	
+	const STATUS_HAISHIN = '配信中';
+	const STATUS_TEISHI = '配信停止';
+	const STATUS_KAIJO = '解除';
+	const STATUS_SAKUJO = '削除';
+	const STATUS_ERROR_TEISHI = 'エラー停止';
+	
+	public function changeStatus($id,$status)
+	{
+		if(
+			self::STATUS_HAISHIN != $status &&
+			self::STATUS_TEISHI != $status &&
+			self::STATUS_KAIJO != $status &&
+			self::STATUS_SAKUJO != $status &&
+			self::STATUS_ERROR_TEISHI != $status
+		){
+			return false;
+		}
+		
+		$options = array('encoding' => 'UTF-8', 'escaping'=>'markup');
+		$params = array($this->access_token, (int)$id, array('status'=>$status));
+		$request = xmlrpc_encode_request('contact.detailUpdate', $params, self::REQUEST_OPTIONS);
+		$context = $this->makeContext($request);
+		$file = file_get_contents("https://api.bme.jp/xmlrpc/1.0", false, $context);
+		$data = xmlrpc_decode($file);
+		if($this->checkError($data)){
+			return false;
+		}
+		return $data;
+	}
+	
+	
+	private function checkError($data){
+		if($data==-3) {
+			$this->errors[] = ['code'=>-3,'message'=>'アドレス重複'];
+			 return true;
+		}else if($data==-2) {
+			$this->errors[] = ['code'=>-2,'message'=>'登録上限'];
+			return true;
+		}else if($data==-1) {
+			$this->errors[] = ['code'=>-1,'message'=>'パラメータ不正'];
+			return true;
+		}else if($data==0) {
+			$this->errors[] = ['code'=>0,'message'=>'登録処理に失敗'];
+			return true;
+		}
+		return false;
+	}
+	
+	public function getErrors(){
+		return $this->errors;
+	}
+	
+	
 }
--- test/BlaynMailTest.php
+++ test/BlaynMailTest.php
@@ -1,61 +1,88 @@
 <?php
+
 namespace takamoriyamashiro\Blayn\Tests;
+
 use Dotenv\Dotenv;
 use PHPUnit\Framework\TestCase;
 use takamoriyamashiro\Blayn\BlaynMail;
 
-class BlaynMailTest extends TestCase{
+class BlaynMailTest extends TestCase
+{
 	
 	private $bm;
-
+	
 	public function setUp()
-    {
-	    $dotenv = new Dotenv(dirname(__DIR__));
-	    $dotenv->load();
-	    
-    }
-    
-    private function login(){
+	{
+		$dotenv = new Dotenv(dirname(__DIR__));
+		$dotenv->load();
+		
+	}
 	
-	    if(empty($this->bm)) {
-		    $this->bm = new BlaynMail(
-			    getenv('BLAYNMAIL_ID'),
-			    getenv('BLAYNMAIL_PASSWORD'),
-			    getenv('BLAYNMAIL_APIKEY')
-		    );
-		    $this->assertTrue(!empty($this->bm->getToken()));
-	    }
-    }
-
-    public function testConnect(){
+	private function login()
+	{
+		
+		if (empty($this->bm)) {
+			$this->bm = new BlaynMail(
+				getenv('BLAYNMAIL_ID'),
+				getenv('BLAYNMAIL_PASSWORD'),
+				getenv('BLAYNMAIL_APIKEY')
+			);
+			$this->assertTrue(!empty($this->bm->getToken()));
+		}
+	}
 	
-	    $this->bm = new BlaynMail(
-		    getenv('BLAYNMAIL_ID'),
-		    getenv('BLAYNMAIL_PASSWORD'),
-		    'xxxxxxxxxxxxxxx'
-	    );
-	    $this->assertTrue($this->bm->getToken() == 0);
+	private function loginError()
+	{
+		
+		$this->bm = new BlaynMail(
+			getenv('BLAYNMAIL_ID'),
+			getenv('BLAYNMAIL_PASSWORD'),
+			'xxxxxxxxxxxxxxx'
+		);
+		$this->assertTrue($this->bm->getToken() == 0);
+	}
 	
-	    $this->login();
 	
-	    $this->assertTrue($this->bm->logout());
+	public function testConnect()
+	{
+		
+		$this->loginError();
+		
+		$this->login();
+		
+		$this->assertTrue($this->bm->logout());
+		
+	}
 	
-    }
-    
-    public function testAddUser(){
+	public function testAddUser()
+	{
+		
+		$this->login();
+		
+		$email = getenv('BLAYNMAIL_EMAIL');
+		$group = getenv('BLAYNMAIL_GROUP');
+		
+		$result = $this->bm->addUser($email, false);
+		$this->assertFalse($result);
+		
+		$result = $this->bm->addUser('', $group);
+		$this->assertFalse($result);
+		
+		$retult = $this->bm->addUser($email, $group);
+		$this->assertTrue(is_numeric($retult));
+	}
 	
-	    $this->login();
 	
-	    $email = 'yamashiro.develop@gmail.com';
-	    $this->bm->addUser(
-		    $email,
-	    );
-	    
-//	    $this->assertTrue(true);
-    	
-    	
-    }
-
-    
-    
+	
+	public function testChangeStatus()
+	{
+		$this->login();
+		$id = getenv('BLAYNMAIL_USER_ID');
+		$email = getenv('BLAYNMAIL_EMAIL');
+		$result = $this->bm->changeStatus($id,BlaynMail::STATUS_ERROR_TEISHI);
+		$this->assertTrue(is_numeric($result));
+	}
+	
+	
+	
 }
